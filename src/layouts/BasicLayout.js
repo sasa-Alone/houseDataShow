8f7ad865abb8f116e44bb3b9a1c5800716c42684
alone <1157450536@qq.com>
2019-05-25 23:38:42 +0800

    test

--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -65,7 +65,7 @@ class BasicLayout extends Component {
             <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} onClick={this.handleClick}>
               <Menu.Item key="1" >
                 <Icon type="user" />
-                <span>房源搜索</span>
+                <span>房源搜索shishikannengbunegnyong </span>
               </Menu.Item>
               <Menu.Item key="2" >
                 <Icon type="video-camera" />
